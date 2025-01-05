18458c65485f37882dd37bcf0b81498977236fad
desiddd <desislava996@gmail.com>
2025-01-05 14:18:36 +0200

    Add files via upload

---
+++ pen4o.js
@@ -0,0 +1,11 @@
+function pen4o(a, b, c) {
+    if (a < b +c, b < a + c, c < a +b ){
+        console.log(`The Triangle of Truth with sides ${a}, ${b}, and ${c} has been validated. Pen4o, you may begin your journey!`);
+    }
+
+    else {
+        console.log(`The Triangle of Truth with sides ${a}, ${b}, and ${c} is invalid. Pen4o remains stuck!`);
+    }
+}
+
+pen4o(1, 2, 3);
